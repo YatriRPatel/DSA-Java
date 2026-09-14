f77d7bd7e12091d39b06a1a548b2bd0d56c782b8
YatriRPatel <161314011+YatriRPatel@users.noreply.github.com>
2026-09-14 09:20:29 +0530

    Time: 1 ms (61.11%), Space: 42.8 MB (49.77%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int score = 0;
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '('){
+                stack.push(score);
+                score = 0;
+            }else{
+                score = stack.pop() + Math.max(score*2,1);
+            }
+        }
+        return score;
+        
+    }
+}
