82046679183c15620d2cf50107732a1e1445f54c
YatriRPatel <161314011+YatriRPatel@users.noreply.github.com>
2026-09-16 09:04:47 +0530

    Time: 5 ms (97.09%), Space: 46.6 MB (96.23%) - LeetHub

---
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -0,0 +1,49 @@
+class CustomStack {
+    private int[] stack;
+    private int[] inc;
+    private int top;
+
+    public CustomStack(int maxSize) {
+        stack = new int[maxSize];
+        inc = new int[maxSize];
+        top = -1;
+    }
+    
+    public void push(int x) {
+        if(top < stack.length - 1){
+            top++;
+            stack[top]=x;
+        }
+        
+    }
+    
+    public int pop() {
+        if(top ==-1){
+            return -1;
+        }
+
+        int result = stack[top] + inc[top];
+        if(top>0){
+            inc[top-1]+=inc[top];
+        }
+        inc[top]=0;
+        top--;
+
+        return result;
+    }
+    
+    public void increment(int k, int val) {
+        int idx = Math.min(k-1,top);
+        if(idx>=0){
+            inc[idx]+=val;
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack obj = new CustomStack(maxSize);
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * obj.increment(k,val);
+ */
